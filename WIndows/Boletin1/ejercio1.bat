846df8911c22055d7acf74bf5015228a698b527c
tomaspineiro <tomaspial95@gmail.com>
2022-11-25 13:01:09 +0100

    ejercio1 Listo

--- WIndows/Boletin1/ejercio1.bat
+++ WIndows/Boletin1/ejercio1.bat
@@ -4,3 +4,11 @@ chcp 1252 1>nul
 rem Crear un programa que pida ao usuario duas variables:"numero" e "otronumero" e mostre co comando boto o resultado da suma, a multiplicación e o resto do primeiro polo segundo.
 
 
+set /p  num1=Numero 1: 
+set /p num2=Numero 2: 
+
+set /a suma= %num1% + %num2%
+set /a multiplicacion= %num1% * %num2%
+set /a resto= !num1! % !num2!
+
+echo  Suma: %suma%, multiplicación: %multiplicacion%, Resto:  %resto%
