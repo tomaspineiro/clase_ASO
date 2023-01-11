d645808a7f80aed6afd5f8b05f8d0b7397b4383e
tomaspineiro <tomaspial95@gmail.com>
2023-01-11 11:03:33 +0100

    trash

--- WIndows/Boletin1/trash.bat
+++ WIndows/Boletin1/trash.bat
@@ -1,11 +1,39 @@
 @echo off
 chcp 1252 1>nul
 
+:: declarado baribles.
+set trash=%userprofile%\trash
+set archivo=%1
+
 rem Crea un script chamado trash.bat que se encargará de asegurarse de que o directorio trash existe no directorio persoal do usuario actual. Se o directorio trash non existe o perfil do usuario actual, entón deberá crealo. 
+
+
+if not exist %trash% (
+    mkdir %trash%
+) 
+
 
 rem Ademais debe mover o ficheiro, cuxa roteiro se pasa por parámetro, ao directorio trash do usuario actual.
 
+
+:: comprobamos que mete paramento 
+if "%archivo%"=="" (
+    echo tienes que indicar un paramentro
+    goto fin
+)
+:: comprobamos que el fichero existe
+if not exist %archivo% (
+    echo El paramentro no existe 
+    goto fin
+)
+
+
+move %archivo% %trash%
+
+
 rem Ademais debe mover o ficheiro, cuxa roteiro se pasa por parámetro, ao directorio trash do usuario actual.Débese pasar por parámetro o roteiro do ficheiro para mover. Se non se pasa por parámetro ningún valor entón deberase mostrar unha mensaxe de erro e terminar co script.
+
+
 
 rem O script debe comprobar que existe o ficheiro cuxa roteiro se pasou por parámetro. Se o ficheiro non existe, deberá mostrar unha mensaxe de erro adecuado e terminar a execución do script.
 
@@ -13,6 +41,4 @@ rem O script tamén debe asegurarse de que o directorio trash existe no director
 
 rem Unha vez realizadas as comprobacións moverá o ficheiro pasado por parámetro ao directorio trash.
 
-
-if exist 
-
+:fin
