4d46edeed15ec55856b7714541a5a850a709fbfe
tomaspineiro <tomaspial95@gmail.com>
2023-01-12 13:11:44 +0100

    acabaado el boletin 1

--- WIndows/Boletin1/trash.bat
+++ WIndows/Boletin1/trash.bat
@@ -1,20 +1,20 @@
 @echo off
 chcp 1252 1>nul
 
+rem Crea un script chamado trash.bat que se encargará de asegurarse de que o directorio trash existe no directorio persoal do usuario actual. Se o directorio trash non existe o perfil do usuario actual, entón deberá crealo. 
+rem Ademais debe mover o ficheiro, cuxa roteiro se pasa por parámetro, ao directorio trash do usuario actual.
+rem Débese pasar por parámetro o roteiro do ficheiro para mover. Se non se pasa por parámetro ningún valor entón deberase mostrar unha mensaxe de erro e terminar co script.
+rem O script debe comprobar que existe o ficheiro cuxa roteiro se pasou por parámetro. Se o ficheiro non existe, deberá mostrar unha mensaxe de erro adecuado e terminar a execución do script.
+rem O script tamén debe asegurarse de que o directorio trash existe no directorio persoal ou perfil do usuario actual. Se non existe, deberá crealo.
+rem Unha vez realizadas as comprobacións moverá o ficheiro pasado por parámetro ao directorio trash.
+
 :: declarado baribles.
 set trash=%userprofile%\trash
 set archivo=%1
 
-rem Crea un script chamado trash.bat que se encargará de asegurarse de que o directorio trash existe no directorio persoal do usuario actual. Se o directorio trash non existe o perfil do usuario actual, entón deberá crealo. 
-
-
 if not exist %trash% (
     mkdir %trash%
 ) 
-
-
-rem Ademais debe mover o ficheiro, cuxa roteiro se pasa por parámetro, ao directorio trash do usuario actual.
-
 
 :: comprobamos que mete paramento 
 if "%archivo%"=="" (
@@ -27,18 +27,6 @@ if not exist %archivo% (
     goto fin
 )
 
-
 move %archivo% %trash%
-
-
-rem Ademais debe mover o ficheiro, cuxa roteiro se pasa por parámetro, ao directorio trash do usuario actual.Débese pasar por parámetro o roteiro do ficheiro para mover. Se non se pasa por parámetro ningún valor entón deberase mostrar unha mensaxe de erro e terminar co script.
-
-
-
-rem O script debe comprobar que existe o ficheiro cuxa roteiro se pasou por parámetro. Se o ficheiro non existe, deberá mostrar unha mensaxe de erro adecuado e terminar a execución do script.
-
-rem O script tamén debe asegurarse de que o directorio trash existe no directorio persoal ou perfil do usuario actual. Se non existe, deberá crealo.
-
-rem Unha vez realizadas as comprobacións moverá o ficheiro pasado por parámetro ao directorio trash.
 
 :fin
